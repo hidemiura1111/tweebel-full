1f404963dc487bcabe5d3591dacc6f267d5dbe9c
hidems <hidemiura.arms@gmail.com>
2020-08-12 00:19:47 +0200

    first commit

---
+++ src/resources/views/_tweet.blade.php
@@ -0,0 +1,21 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+        src="https://i.pravatar.cc/40"
+        alt=""
+        class="rounded-full mr-2"
+    >
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+
+        <p class="text-sm">
+            Before we can dive into writing the core logic, let's first set aside fifteen minutes
+            or so to design the main timeline page, using Tailwind.Before we can dive into writing
+            the core logic, let's first set aside fifteen minutes or so to design the main timeline
+            page, using Tailwind.Before we can dive into writing the core logic, let's first set aside
+            fifteen minutes or so to design the main timeline page, using Tailwind.
+        </p>
+    </div>
+</div>
